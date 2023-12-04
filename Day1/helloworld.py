69999bdf15d69b375a04b6527649363aac3fa8c8
mauwal07 <mauwal07@gmail.com>
2023-12-04 19:06:17 +0100

    add day1 assignment

---
+++ Day1/helloworld.py
@@ -0,0 +1,32 @@
+# Exercise: Level 2
+#1. Checking python version 
+
+# python --version.    # not working, and therefore delibrately commented the code.
+
+#2. Arithmetic operations
+
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+#3. Strings
+
+print("Muhammad Auwal")  # My name as string
+print("Abubakar")  # My family name as string
+print("Nigeria")  # My country as string
+print("I am enjoying 30 days of python")  # As string
+
+#4. Checking data types
+
+print(type(10))          # Int
+print(type(9.8))        # Float
+print(type(3.14))        # Float
+print(type(4 - 4j))      # Complex number
+print(type(['Asabeneh', 'Python', 'Finland']))  # List
+print('Muhammad Auwal')  # string
+print('Abubakar')        # string
+print('Nigeria')         # string
